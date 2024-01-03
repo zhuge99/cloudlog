4f1c80e7451e586a44be7e70e9b0052851f367d1
zhuge99 <155508192+zhuge99@users.noreply.github.com>
2024-01-03 12:38:19 +0000

    feat: Add and update cloud logging configuration
    Add import for 'fmt', update cloudlog configuration functions

--- example/main.go
+++ example/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/zhuge99/cloudlog"
 )
 
 func main() {
+
 	cloudlog.DCL_addStdout()
-	cloudlog.DCL_addLocalFileDefault()
-	cloudlog.DCL_addLogflare("", "")
+	err := cloudlog.DCL_addLocalFileDefault()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = cloudlog.DCL_addLogflare("d0399bff-7fc7-4874-a572-05309021d853", "WoMn49mFQDkh")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	cloudlog.DCL_Info("hello info")
 	cloudlog.DCL_Error("hello error")
 }
